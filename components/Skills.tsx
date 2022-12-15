a211478cd47c16b61ede69189ea854615827a9e1
RAMZI <ramzighadar5@gmail.com>
2022-12-15 02:39:32 +0100

    Added new file

---
+++ components/Skills.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <div className="flex flex-col">
+      <div className="pb-2 text-center font-inter">
+        <h2 className="text-4xl font-bold">Skills</h2>
+      </div>
+      <div className="grid items-center grid-cols-4 gap-4 font-inter">
+        <div className="w-[250px] h-[150px] bg-[#1e293b] p-4 rounded-lg flex justify-center items-center text-3xl font-bold">
+          mean-stuck
+        </div>
+        <div className="w-[250px] h-[150px] bg-[#1e293b] p-4 rounded-lg flex justify-center items-center text-3xl font-bold">
+          NEXT.JS
+        </div>
+        <div className="w-[250px] h-[150px] bg-[#1e293b] p-4 rounded-lg flex justify-center items-center text-3xl font-bold">
+          angular
+        </div>
+        <div className="w-[250px] h-[150px] bg-[#1e293b] p-4 rounded-lg flex justify-center items-center text-3xl font-bold">
+          java-script
+        </div>
+        <div className="w-[250px] h-[150px] bg-[#1e293b] p-4 rounded-lg flex justify-center items-center text-3xl font-bold">
+          mangodb
+        </div>
+        <div className="w-[250px] h-[150px] bg-[#1e293b] p-4 rounded-lg flex justify-center items-center text-3xl font-bold">
+          node-js
+        </div>
+        <div className="w-[250px] h-[150px] bg-[#1e293b] p-4 rounded-lg flex justify-center items-center text-3xl font-bold">
+          TYPESCRIPT
+        </div>
+        <div className="w-[250px] h-[150px] bg-[#1e293b] p-4 rounded-lg flex justify-center items-center text-3xl font-bold">
+          html & css
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
---
+++ testnextjsapp/pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+import * as dotenv from 'dotenv'
+
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
